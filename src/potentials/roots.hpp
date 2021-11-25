39a682581f817c0edd3ce0e2c51587dd0515e2f3
moritzhof <moritz.t.hof@gmail.com>
2021-11-25 11:08:25 +0100

    roots class

---
+++ src/potentials/roots.hpp
@@ -0,0 +1,32 @@
+
+namespace qwv{
+ namespace discretization{
+ 
+    template<typename T>
+    class roots{
+    public:
+        
+        roots() = default;
+        roots(std::size_t N) : N_(N){
+            result.reserve(N);
+         for(auto i :std::views::iota(std::size_t(0)) | std::views::take(N)){
+            this->result.push_back(std::cos(M_PI*(2*i+1)/(2*N)));
+            }
+        }
+        
+        ~roots() = default;
+        
+        auto& operator[](auto i ){
+            return this->result[i];
+        }
+        
+        void free(){
+            return this->result.resize(0);
+        }
+    private:
+        std::size_t N_;
+        std::vector<T> result{};
+    };
+    
+  } //end of discretization namespace
+} // end of qwv namespace
